c5355936a8c541e0283d0cba9b0dd2b3fc109bac
Gawah Chan <gawahchan@gmail.com>
2019-09-30 22:02:46 +0100

    Updates

---
+++ precache-manifest.003077ae8bb17b91dec1eb5b55f5c810.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc701a55b3681fba8948dd74e361ca56",
+    "url": "/JSON-Placeholder-Posts/index.html"
+  },
+  {
+    "revision": "5fa393279ea97b31ceb0",
+    "url": "/JSON-Placeholder-Posts/static/css/main.8102c22c.chunk.css"
+  },
+  {
+    "revision": "67cf963ec13e828774d2",
+    "url": "/JSON-Placeholder-Posts/static/js/2.a058e502.chunk.js"
+  },
+  {
+    "revision": "5fa393279ea97b31ceb0",
+    "url": "/JSON-Placeholder-Posts/static/js/main.23910f08.chunk.js"
+  },
+  {
+    "revision": "25ba19f30562b4f242e8",
+    "url": "/JSON-Placeholder-Posts/static/js/runtime~main.b6f49df1.js"
+  }
+]);
